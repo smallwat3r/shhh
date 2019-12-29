9a290ae9eb122e6029b6c62605682e56fdffc0da
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2019-12-29 14:53:17 +0000

    rm sdist

--- shhh/static/sdist/8026d8f691bd6fb1bd7221e981ec830d.js
+++
@@ -1,3 +0,0 @@
-
-$(document).ready(function(){$('#secretSender').on('submit',function(event){event.preventDefault();blockUI();$.ajax({url:'/api/send',data:$('#secretSender').serialize(),type:'POST',success:function(d){$('#linkPop').fadeIn(1000);var domain=window.location.hostname;if(domain=='0.0.0.0'){$('#linkDetail').empty().append(window.location.hostname+':5000/read?slug='+d.slug);}else{$('#linkDetail').empty().append(window.location.hostname+'/read?slug='+d.slug);}
-$('#linkExpire').empty().append(d.expires);},complete:function(){$.unblockUI();}});});});
--- shhh/static/sdist/e2469146b175a66e49b3b76e5253ab67.js
+++
@@ -1,2 +0,0 @@
-
-$(document).ready(function(){$('#secretSender').on('submit',function(event){event.preventDefault();blockUI();$.ajax({url:'/api/send',data:$('#secretSender').serialize(),type:'POST',success:function(d){$('#linkPop').fadeIn(1000);$('#linkDetail').empty().append(window.location.hostname+'/read?slug='+d.slug);$('#linkExpire').empty().append(d.expires);},complete:function(){$.unblockUI();}});});});
