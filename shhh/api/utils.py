2daf6a306cb4c4b225687efb0e41f37e05fda020
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-05-02 03:18:39 +0100

    Linting

--- shhh/api/utils.py
+++ shhh/api/utils.py
@@ -4,17 +4,27 @@ import requests
 
 
 def pwned_password(passphrase):
-    """Check passphrase with Tory's Hunt haveibeenpwned API."""
+    """Check passphrase with Tory's Hunt haveibeenpwned API.
+
+    Query the API to check if the passphrase has already been pwned in the
+    past. If it has, returns the first match from generator, else returns
+    False.
+
+    """
     hasher = hashlib.sha1()
     hasher.update(passphrase.encode("utf-8"))
     digest = hasher.hexdigest().upper()
 
-    r = requests.get(f"https://api.pwnedpasswords.com/range/{digest[:5]}",
-                     timeout=5)
+    endpoint = f"https://api.pwnedpasswords.com/range"
+    r = requests.get(f"{endpoint}/{digest[:5]}", timeout=5)
     r.raise_for_status()
 
     # yapf: disable
-    pw_list = r.text.split("\n")
     return next(
-        (int(e.split(":")[1]) for e in pw_list
-         if (e.split(":")[0] == digest[5:])), False)
+        (
+            int(e.split(":")[1])
+            for e in r.text.split("\n")
+            if e.split(":")[0] == digest[5:]
+        ),
+        False
+    )
