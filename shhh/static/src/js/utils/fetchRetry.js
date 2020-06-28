bd31463f4d612476cdd9fc9e40c75590068aad83
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-06-28 22:35:22 +0100

    Expand retrying timeout

--- shhh/static/src/js/utils/fetchRetry.js
+++ shhh/static/src/js/utils/fetchRetry.js
@@ -1,8 +1,9 @@
-export default function fetchRetry(url, options = {}, retries = 4, backoff = 300) {
+export default function fetchRetry(url, options = {}, retries = 6, backoff = 300) {
   return fetch(url, options)
     .then(res => {
       if (retries > 0 && res.status === 500) {
         setTimeout(() => {
+          console.log("Retrying...")
           return fetchRetry(url, options, retries - 1, backoff * 2);
         }, backoff);
       }
