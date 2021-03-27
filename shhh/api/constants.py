a4e533bcfad405a9d72aae4f872131a32de1856f
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-03-27 02:04:59 +0000

    Format constants

--- shhh/api/constants.py
+++ shhh/api/constants.py
@@ -1,15 +1,13 @@
+# pylint: disable=line-too-long
 import enum
 
 
 class Messages(enum.Enum):
     """Message constants."""
 
-    NOT_FOUND = (
-        "Sorry, we can't find a secret, it has expired, been deleted or has already been read."
-    )
-    EXCEEDED = (
-        "The passphrase is not valid. You've exceeded the "
-        "number of tries and the secret has been deleted."
-    )
+    # fmt: off
+    NOT_FOUND = "Sorry, we can't find a secret, it has expired, been deleted or has already been read."
+    EXCEEDED = "The passphrase is not valid. You've exceeded the number of tries and the secret has been deleted."
     INVALID = "Sorry the passphrase is not valid. Number of tries remaining {remaining}."
     CREATED = "Secret successfully created."
+    # fmt: on
