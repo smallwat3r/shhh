f458fd4f207b40e5f98eec7fb60fe8ca8ba69a36
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2021-05-15 01:26:18 +0100

    Add gunicorn workers from multiprocessing

--- gunicorn.heroku.py
+++ gunicorn.heroku.py
@@ -1,6 +1,10 @@
+import multiprocessing
+
+
 def when_ready(server):  # pylint: disable=unused-argument
     """Called just after the server is started."""
     open("/tmp/app-initialized", "w").close()
 
 
 bind = "unix:///tmp/nginx.socket"
+workers = multiprocessing.cpu_count() * 2 + 1
