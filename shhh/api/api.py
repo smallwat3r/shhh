9f65afc8ba9c97f591e5b17a868757d45cc3099f
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2023-09-30 15:58:30 +0100

    Better handler structure

--- shhh/api/api.py
+++ shhh/api/api.py
@@ -3,30 +3,23 @@ from __future__ import annotations
 import functools
 from typing import TYPE_CHECKING
 
-from flask import Blueprint, make_response
+from flask import Blueprint
 from flask.views import MethodView
 from webargs.flaskparser import abort, parser, use_kwargs
 
-from shhh.api import handlers
+from shhh.api.handlers import ErrorHandler, ReadHandler, WriteHandler
 from shhh.api.schemas import ReadRequest, WriteRequest
 
 if TYPE_CHECKING:
-    from http import HTTPStatus
     from typing import NoReturn
 
     from flask import Response
     from marshmallow import ValidationError
 
-    from shhh.api.schemas import CallableResponse
-
-
-def _handle(response: CallableResponse, code: HTTPStatus) -> Response:
-    return make_response(response(), code)
-
 
 @parser.error_handler
 def handle_parsing_error(err: ValidationError, *args, **kwargs) -> NoReturn:
-    abort(_handle(*handlers.parse_error(err)))
+    abort(ErrorHandler(err).make_response())
 
 
 body = functools.partial(use_kwargs, location="json")
@@ -37,11 +30,11 @@ class Api(MethodView):
 
     @query(ReadRequest())
     def get(self, *args, **kwargs) -> Response:
-        return _handle(*handlers.read(*args, **kwargs))
+        return ReadHandler(*args, **kwargs).make_response()
 
     @body(WriteRequest())
     def post(self, *args, **kwargs) -> Response:
-        return _handle(*handlers.write(*args, **kwargs))
+        return WriteHandler(*args, **kwargs).make_response()
 
 
 api = Blueprint("api", __name__, url_prefix="/api")
